61472dc56348a119e7db30cb1908a0923f99b616
unknown <alexandrisanas@gmail.com>
2019-01-08 17:10:36 +0200

    init

--- Program.cs
+++ Program.cs
@@ -1,37 +1,21 @@
 ﻿using System;
 
-interface I1
+public abstract class Customer
 {
-    void interMethod();
-}
-
-interface I2
-{
-    void interMethod();
+    public abstract void Print();
 }
 
 
-
-
-public class Program : I1, I2
+public class Program : Customer
 {
 
-    void I1.interMethod()
+    public override void Print()
     {
-        System.Console.WriteLine("interface 1 method");
+        System.Console.WriteLine("print the abstract now");
     }
-    void I2.interMethod()
+    public static void Main(string[] args)
     {
-        System.Console.WriteLine("interface 2 method");
-    }
-    public static void Main()
-    {
-        // When the class inherits from 2 interfaces and both intefaces have the same
-        // method name explicit interface implementation is used to implement the method
         Program P = new Program();
-        // if the P.Interface needs to be called -- default inteface implementation
-        // change the first method to public void InterfaceMethod()
-        ((I1)P).interMethod();
-        ((I2)P).interMethod();        
+        P.Print();
     }
 }
