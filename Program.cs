6019c9718fa9732329219fbc518f3189067858e2
unknown <alexandrisanas@gmail.com>
2019-01-08 15:29:20 +0200

    init

--- Program.cs
+++ Program.cs
@@ -1,34 +1,12 @@
 ﻿using System;
 
 
-interface ICustomer
-{
-    void Print();
-}
-interface ICustomer2 : ICustomer
-{
-    void Print2();
-}
 
-class Customer : ICustomer2
-{
-    public void Print()
-    {
-        System.Console.WriteLine("Interface print method");
-    }
-    public void Print2()
-    {
-        System.Console.WriteLine("Interface Print 2");
-    }
-
-}
 
 public class Program
 {
-    public static void Main()
+    public static void main()
     {
-        ICustomer C1 = new Customer();
-        // only the print method of the first interface is available
-        C1.Print();
+        System.Console.WriteLine('hey');
     }
 }
