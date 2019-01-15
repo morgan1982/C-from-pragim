742a4957c41c30bb1c27cf87c8ed3b28b37f184f
unknown <alexandrisanas@gmail.com>
2019-01-15 10:51:26 +0200

    init

--- Program.cs
+++ Program.cs
@@ -3,22 +3,15 @@
 public class Customer
 {
     public string name { get; set; }
-    public int gender { get; set; }
+    public string gender { get; set; }
 
-    public static string GetGender(int gen)
-    {
-        switch(gen)
-        {
-            case 0:
-                return "Unknown";
-            case 1:
-                return "Male";
-            case 2:
-                return "Female";
-            default:
-                return "invalid data";
-        }
-    }
+}
+
+public enum Gender
+{
+    Male,
+    Female,
+    Unknown
 }
 
 
@@ -32,22 +25,22 @@ class Program
         customers[0] = new Customer
         {
             name = "Mark",
-            gender = 1
+            gender = Enum.GetName(typeof(Gender), Gender.Male)
         };
         customers[1] = new Customer
         {
             name = "tom",
-            gender = 1
+            gender = Enum.GetName(typeof(Gender), Gender.Male)
         };
         customers[2] = new Customer
         {
             name = "tonya",
-            gender = 2
+            gender = Enum.GetName(typeof(Gender), Gender.Female)
         };
 
         foreach (var customer in customers)
         { 
-            System.Console.WriteLine("name: {0} genre: {1}", customer.name, Customer.GetGender(customer.gender));
+            System.Console.WriteLine("name: {0} genre: {1}", customer.name, customer.gender);
         }
 
 
