e64f892eb2b83ca5548cc282218a8d2ad17e6b0d
unknown <alexandrisanas@gmail.com>
2019-02-27 18:54:34 +0200

    clean

--- Program.cs
+++ Program.cs
@@ -159,14 +159,9 @@ namespace AngleShardDemo1
 
                         }
                     }
-
                     element.SetAttribute("style", elementString.ToString());
-
-
                 }
             }
-
-
         }
 
         static void TestMod(IHtmlDocument document)
@@ -195,122 +190,8 @@ namespace AngleShardDemo1
                 description.SetAttribute("style", splitStyleOfParent[1]);
             }
 
-
         }
 
-
-        static void UserModificationsSanitizer(IElement element)
-        {
-            if (element == null)
-            {
-                return;
-            }
-            //var span = document.CreateElement("span");
-            //span.SetAttribute("style", "font-size:10px;");
-            //span.TextContent = "hello there";
-
-            //descriptionElement.Append(span);
-
-            string findFontSize = "font-size([^px]*px)"; // 3
-
-            // bold -> match the <b> -> create font-wieght: 700 attibute
-
-            Regex fontSizeFinder = new Regex(findFontSize);
-            Regex pickParent = new Regex("<(.*?)>");
-
-            string innerMatch = fontSizeFinder.Match(element.InnerHtml).Value; // font-size of inner
-            // selects the outer element  Consider to use stringBuilder
-            string outerMatch = pickParent.Match(element.OuterHtml).Value; // the outer element it self to sting
-            // has to find if it as font-size first and then make it general
-
-            // function to check if certain styles are in existance.. don't invoke the engine if it is not needed
-            bool parentHasStyle = outerMatch.Contains("style");
-
-            // BOOLS TO CHECK IF VALUE EXISTS
-            bool parentHasFontSize = outerMatch.Contains("font-size");
-
-
-            StringBuilder outer = new StringBuilder(element.OuterHtml);
-
-            if (parentHasFontSize)
-            {
-                string outerStyle = fontSizeFinder.Match(element.OuterHtml).Value; // finds the font-size in the outer html
-
-                outer.Replace(outerStyle, innerMatch);
-                element.OuterHtml = outer.ToString();
-
-            }
-            else
-            {
-                element.SetAttribute("style", $"{ innerMatch }"); // have to change destoys the old attributes
-            }
-        }
-
-
-
-
-
-
-        // ANGLE PARSER
-        static string Parser(string template)
-        {
-            var parser = new HtmlParser();
-            var document = parser.ParseDocument(template);
-
-            // ELEMENTS FOR MOD
-            var descriptionElements = document.GetElementsByName("__DESCRIPTION__");
-            var titles = document.GetElementsByName("__TITLE__");
-
-            // MODIFICATIONS
-            //description[0].InnerHtml = "einai poly kalo content";
-            titles[0].InnerHtml = "hello there rooster!";
-            //titles[1].InnerHtml = "hello there rooster again!";
-
-            var description = descriptionElements[0];
-
-            string span = description.InnerHtml; // 1
-            string outer = description.OuterHtml;// 2
-            // select reg
-            //string regStr = "font(.*)px";
-            //string regStr = "/font.\{px}\/";
-            string regStr = "font([^px]*)px"; // 3
-            var reg = new Regex(regStr);
-            var matches = reg.Matches(span);
-            string fontOfParrent = matches[0].ToString(); // 4
-
-            string newOuter = Regex.Replace(outer, regStr, fontOfParrent); // 5
-
-            description.OuterHtml = newOuter;
-            //var attributtes = description[0].Attributes; // [1] return the string of the styles;
-            //var childs = description[0].ChildNodes;
-            ////var span = description[0].FirstChild;
-
-
-
-            string final = document.DocumentElement.OuterHtml;
-
-            return final;
-        }
- 
-        static async Task<List<string>> Parser()
-        {
-            var config = Configuration.Default.WithDefaultLoader();
-            var address = "https://en.wikipedia.org/wiki/List_of_The_Big_Bang_Theory_episodes";
-            var context = BrowsingContext.New(config);
-
-            var document = await context.OpenAsync(address);
-            var cellSelector = "tr.vevent td:nth-child(3)";
-            var cells = document.QuerySelectorAll(cellSelector);
-            var titles = cells.Select(m => m.TextContent).ToList();
-            return titles;
-        }
-        
     }
-    class Employee : IEnumerable
-    {
-        public IEnumerator GetEnumerator()
-        {
-            throw new NotImplementedException();
-        }
-    }
+
 }
